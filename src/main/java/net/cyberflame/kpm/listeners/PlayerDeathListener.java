fbc50f18e08ea2ab542613bd2b04fe03a6c00f9e
CyberFlame <cyberflameu@gmail.com>
2021-06-10 14:25:22 +1200

    bofa

--- src/main/java/net/cyberflame/kpm/listeners/PlayerDeathListener.java
+++ src/main/java/net/cyberflame/kpm/listeners/PlayerDeathListener.java
@@ -1,11 +1,14 @@
 package net.cyberflame.kpm.listeners;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
+
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -20,6 +23,7 @@ public class PlayerDeathListener implements Listener
         Player p = e.getEntity();
         Location l = p.getLocation();
         this.playerMap.put(p.getName(), p.getActivePotionEffects());
+        ItemStack potion = new ItemStack(Material.POTION);
         for (PotionEffect pe : this.playerMap.get(p.getName()))
             p.addPotionEffect(pe);
         // world.dropItemNaturally(player.getLocation(), soupItem);
