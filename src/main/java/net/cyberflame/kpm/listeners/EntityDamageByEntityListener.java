df292b6815ae215c5801c4e0dafbc2f6b595dddb
CyberFlame <cyberflameu@gmail.com>
2021-06-17 11:08:29 +1200

    stuff

--- src/main/java/net/cyberflame/kpm/listeners/EntityDamageByEntityListener.java
+++ src/main/java/net/cyberflame/kpm/listeners/EntityDamageByEntityListener.java
@@ -1,11 +1,13 @@
 package net.cyberflame.kpm.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.projectiles.ProjectileSource;
@@ -18,9 +20,16 @@ public class EntityDamageByEntityListener implements Listener
         if (e.getDamager() instanceof Player && e.getEntity() instanceof Player)
         {
             Player attacker = (Player) e.getDamager();
+            Material attackItem = attacker.getItemInHand().getType();
             if (attacker.hasPotionEffect(PotionEffectType.INVISIBILITY))
             {
                 attacker.removePotionEffect(PotionEffectType.INVISIBILITY);
+            }
+            else if (attackItem == Material.WOOD_HOE || attackItem == Material.GOLD_HOE
+                    || attackItem == Material.STONE_HOE || attackItem == Material.IRON_HOE
+                    || attackItem == Material.DIAMOND_HOE)
+            {
+
             }
         } else if (e.getDamager() instanceof Snowball)
             if (e.getEntity() instanceof Player)
