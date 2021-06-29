30631defffcc1d2a8a2dfa7b850dd894f6720cd7
CyberFlame <cyberflameu@gmail.com>
2021-06-29 12:42:24 +1200

    potential buildmode functionality fix

--- src/main/java/net/cyberflame/kpm/commands/BuildModeCommand.java
+++ src/main/java/net/cyberflame/kpm/commands/BuildModeCommand.java
@@ -36,13 +36,12 @@ public class BuildModeCommand implements CommandExecutor
                         plugin.setBuildEnabled(Bukkit.getPlayer(args[0]).getUniqueId());
                         Utils.sendMessage(sender, (plugin.getBuildEnabled(target.getUniqueId()) ? Utils.BUILD_OTHER_TOGGLE_ON.replaceAll("%player%", targetname) : Utils.BUILD_OTHER_TOGGLE_OFF.replaceAll("%player%", targetname)));
                         Utils.sendMessage(target, (plugin.getBuildEnabled(target.getUniqueId()) ? Utils.BUILD_TOGGLE_ON.replaceAll("%player%", targetname) : Utils.BUILD_TOGGLE_OFF.replaceAll("%player%", targetname)));
-                        return true;
                     }
                 else
                     {
                         Utils.sendMessage(sender, Utils.UNKNOWN_PLAYER.replaceAll("%player%", args[0]));
-                        return true;
                     }
+                return true;
             }
         Player player = (Player) sender;
         plugin.setBuildEnabled(player.getUniqueId());
